bb5ed5dbf2f8c3e17af39933dbc42a71c18155df
Diego Griffon <griffondiego@gmail.com>
2026-01-29 10:11:32 -0400

    Update 10- Juego del Halcón y la Paloma.R

--- 10- Juego del Halcón y la Paloma.R
+++ 10- Juego del Halcón y la Paloma.R
@@ -1,5 +1,4 @@
 
-
 ##################################################
 #          Teoría de juegos evolutivos           #
 #         Juego del Halcón y la Paloma           #
@@ -13,8 +12,8 @@
 
 
 # Parámetros del modelo
-V <- 9  # Valor del recurso
-C <- 3  # Costo de la pelea
+V <- 6  # Valor del recurso
+C <- 2  # Costo de la pelea
 
 # Condiciones iniciales:
 Frecuencia_Inicial_Halcones<- 0.1  # Frecuencia inicial de halcones
@@ -22,7 +21,7 @@ Frecuencia_Inicial_Halcones<- 0.1  # Frecuencia inicial de halcones
 # Tiempo a simular:
 Generaciones <- 10  # Número de generaciones
 
-# Función para calcular el pago esperado:
+# Función para calcular el pago esperado (Fitness):
 Pagos_Esperados <- function(Frecuencia_Halcones, V, C) {
   Pago_Halcones <- Frecuencia_Halcones * (V - C) / 2 + (1 - Frecuencia_Halcones) * V
   Pago_Palomas <- Frecuencia_Halcones * 0 + (1 - Frecuencia_Halcones) * V / 2
@@ -30,14 +29,14 @@ Pagos_Esperados <- function(Frecuencia_Halcones, V, C) {
 }
 
 # Simulación de la dinámica de la población:
-Simulacion_Juego <- function(initial_Frecuencia_Halcones, V, C, Generaciones) {
+Simulacion_Juego <- function(Frecuencia_Inicial_Halcones, V, C, Generaciones) {
   Frecuencia_Halcones <- numeric(Generaciones)
-  Frecuencia_Halcones[1] <- initial_Frecuencia_Halcones
+  Frecuencia_Halcones[1] <- Frecuencia_Inicial_Halcones
   
   for (gen in 2:Generaciones) {
     Pagos <- Pagos_Esperados(Frecuencia_Halcones[gen - 1], V, C)
-    Pago_Total <- Frecuencia_Halcones[gen - 1] * Pagos[1] + (1 - Frecuencia_Halcones[gen - 1]) * Pagos[2]
-    Frecuencia_Halcones[gen] <- Frecuencia_Halcones[gen - 1] * Pagos[1] / Pago_Total
+    Pago_Promedio <- Frecuencia_Halcones[gen - 1] * Pagos[1] + (1 - Frecuencia_Halcones[gen - 1]) * Pagos[2]
+    Frecuencia_Halcones[gen] <- Frecuencia_Halcones[gen - 1] * Pagos[1] / Pago_Promedio
   }
   
   return(Frecuencia_Halcones)
@@ -47,13 +46,13 @@ Simulacion_Juego <- function(initial_Frecuencia_Halcones, V, C, Generaciones) {
 Frecuencia_Halcones <- Simulacion_Juego(Frecuencia_Inicial_Halcones, V, C, Generaciones)
 
 # Crear un data frame para graficar:
-data <- data.frame(Generacion = 1:Generaciones, Frecuencia_Halcones = Frecuencia_Halcones)
+Datos <- data.frame(Generacion = 1:Generaciones, Frecuencia_Halcones = Frecuencia_Halcones)
 
 # Graficar los resultados:
 
 library(ggplot2)
 
-ggplot(data, aes(x = Generacion, y = Frecuencia_Halcones)) +
+ggplot(Datos, aes(x = Generacion, y = Frecuencia_Halcones)) +
   geom_line(color = "blue") +
   labs(title = "Dinámica del Juego del \n   Halcón y la Paloma",
        x = "Generación",
@@ -63,3 +62,10 @@ ggplot(data, aes(x = Generacion, y = Frecuencia_Halcones)) +
 
 # John Maynard Smith. 1982. Evolution and the Theory 
 # of Games. Cambridge University Press.
+
+# Deducción de la ecuación dinámica para 
+# haploides en tiempo discreto:
+
+# https://n9.cl/ds6qa
+
+
